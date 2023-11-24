f5dc53745cd59f90bb4811d20767d21c34d4d5e5
Samuel Dahunsi <dahunsisamuel1st@gmail.com>
2023-11-24 06:12:23 +0000

    update ui logic

--- ui-frontend/src/App.jsx
+++ ui-frontend/src/App.jsx
@@ -6,20 +6,27 @@ function App() {
   const { account, connectWallet, contract } = useContext(WalletContext);
   const [secret, setSecret] = useState('');
   const [refetch, setRefetch] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [owner, setOwner] = useState('');
 
   useEffect(() => {
     if (contract && refetch) {
       contract.showSecret().then((data) => {
         setSecret(data);
+        setRefetch(false);
       });
     }
-  }, [contract, refetch]);
+  }, [refetch, contract]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
+    setLoading(true);
     const secretValue = e.target.secret.value;
-    contract.setSecret(secretValue).then(() => setRefetch(true));
+    e.target.secret.value = "";
+    const set = await contract.setSecret(secretValue);
+    await set.wait();
+    setLoading(false);
+    setRefetch(true);
   };
 
   const showOwner = async () => {
@@ -34,23 +41,25 @@ function App() {
         <>
           <p>Current Secret: {secret}</p>
 
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="secret">Enter secret</label>
+          <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column", gap: "20px"}}>
+            <div style={{display: "flex", gap: "10px", justifyContent: "center"}}>
+              <label htmlFor="secret">Enter secret:</label>
               <input
                 type="text"
                 name="secret"
                 id="secret"
-                value={secret}
-                onChange={(e) => setSecret(e.target.value)}
+                style={{padding: "5px"}}
               />
             </div>
-            <button type="submit">Update secret</button>
+            <button type="submit">{loading ? "Updating" : "Update secret"}</button>
           </form>
 
-          <p>Owner Address: {owner}</p>
-
+          <div style={{display: "flex", flexDirection: "column"}}>
+          <p style={{marginBottom: "0"}}>Owner Address: </p>
+          <p>{owner}</p>
+          </div>
           <button onClick={showOwner}>Show Owner</button>
+
         </>
       ) : (
         <button onClick={connectWallet}>Connect</button>
